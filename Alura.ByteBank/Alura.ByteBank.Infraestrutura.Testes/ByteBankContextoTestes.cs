322faeaa13d47284335de680bcacc84b2185c929
Marcos Paulo <marcos.poo@gmail.com>
2024-02-20 12:42:40 -0300

    Aula 2 - Teste de integração
    Nessa aula, aprendemos:
    - Criar um novo projeto de teste com xUnit;
    - Criar nosso primeiro teste de integração para validar uma conexão com o serviço de banco de dados;
    - Realizar uma migração que insere algumas informações nas tabelas do banco de dados usando o recurso das migrações com Entity Framework;
    - Utilizar o pacote using Microsoft.Extensions.DependencyInjection; do .NET para criar uma injeção de dependência no construtor da classe de testes.

---
+++ Alura.ByteBank/Alura.ByteBank.Infraestrutura.Testes/ByteBankContextoTestes.cs
@@ -0,0 +1,28 @@
+﻿using Alura.ByteBank.Dados.Contexto;
+using System;
+using Xunit;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public class ByteBankContextoTestes
+    {
+        [Fact]
+        public void TestaConexaoContextoComBDMySQL()
+        {
+            //Arrange
+            var contexto = new ByteBankContexto();
+            bool conectado;
+            //Act
+            try
+            {
+                conectado = contexto.Database.CanConnect();
+            }
+            catch (Exception)
+            {
+                throw new Exception("Não foi possivel conectar a base da dados.");
+            }
+            //Assert
+            Assert.True(conectado);
+        }
+    }
+}
--- Alura.ByteBank/Alura.ByteBank.Infraestrutura.Testes/ClienteRepositorioTestes.cs
+++ Alura.ByteBank/Alura.ByteBank.Infraestrutura.Testes/ClienteRepositorioTestes.cs
@@ -1,30 +1,37 @@
 ﻿using Alura.ByteBank.Dados.Repositorio;
 using Alura.ByteBank.Dominio.Entidades;
+using Alura.ByteBank.Dominio.Interfaces.Repositorios;
+using Microsoft.Extensions.DependencyInjection;
 using Moq;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Alura.ByteBank.Infraestrutura.Testes
 {
     public class ClienteRepositorioTestes
     {
-         private ClienteRepositorio _repositorio;
+        private readonly IClienteRepositorio _clienteRepositorio;
+        private ClienteRepositorio _repositorio;
 
-         [Fact]
+        public ClienteRepositorioTestes()
+        {
+            var servico = new ServiceCollection();
+            servico.AddTransient<IClienteRepositorio, ClienteRepositorio>();
+            var provedor = servico.BuildServiceProvider();
+            _clienteRepositorio = provedor.GetService<IClienteRepositorio>();
+            
+        }
+
+        [Fact]
          public void TestaObterTodosClientes()
         {
             //Arrange
-            _repositorio = new ClienteRepositorio();
-
             //Act
-            List<Cliente> lista = _repositorio.ObterTodos();
+            List<Cliente> lista = _clienteRepositorio.ObterTodos();
 
             //Assert
             Assert.NotNull(lista);
+            Assert.Equal(3, lista.Count);
 
         }
 
